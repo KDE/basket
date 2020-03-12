9a02338bfa60336b7349ff8ac1f85793515ff0e3
Niccol Venerandi <niccolo@venerandi.com>
2020-03-12 10:53:05 +0100

    LIBGIT_VER_MINOR is supposed to be LIBGIT2_VER_MINOR
    Reviewers: ognarb
    
    Reviewed By: ognarb
    
    Differential Revision: https://phabricator.kde.org/D28001

--- src/application.cpp
+++ src/application.cpp
@@ -56,7 +56,7 @@ Application::Application(int &argc, char **argv)
 
 
     #ifdef WITH_LIBGIT2
-        #if LIBGIT2_VER_MAJOR > 0 || (LIBGIT2_VER_MAJOR == 0 && LIBGIT_VER_MINOR >= 22)
+        #if LIBGIT2_VER_MAJOR > 0 || (LIBGIT2_VER_MAJOR == 0 && LIBGIT2_VER_MINOR >= 22)
             git_libgit2_init();
         #else
             git_threads_init();
@@ -67,7 +67,7 @@ Application::Application(int &argc, char **argv)
 Application::~Application()
 {
     #ifdef WITH_LIBGIT2
-        #if LIBGIT2_VER_MAJOR > 0 || (LIBGIT2_VER_MAJOR == 0 && LIBGIT_VER_MINOR >= 22)
+        #if LIBGIT2_VER_MAJOR > 0 || (LIBGIT2_VER_MAJOR == 0 && LIBGIT2_VER_MINOR >= 22)
             git_libgit2_shutdown();
         #else
             git_threads_shutdown();
