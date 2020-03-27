651f38dfa1cc0046b98043aca9a08767e1c13050
Niccolò Venerandi <niccolo@venerandi.com>
2020-03-27 12:47:13 +0100

    Add copyright email
    Reviewers: ognarb
    
    Reviewed By: ognarb
    
    Differential Revision: https://phabricator.kde.org/D28335

--- src/settings_versionsync.h
+++ src/settings_versionsync.h
@@ -1,5 +1,5 @@
 /*
- *   Copyright (C) 2016 by Gleb Baryshev
+ *   Copyright (C) 2016 by Gleb Baryshev <gleb.baryshev@gmail.com>
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
