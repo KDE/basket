7cfde9ad34bac93eb694aa08009dbe0357880f92
Carl Schwan <carl@carlschwan.eu>
2020-04-24 14:38:59 +0200

    feat: update AboutData class

--- src/aboutdata.cpp
+++ src/aboutdata.cpp
@@ -1,21 +1,7 @@
-/***************************************************************************
- *   Copyright (C) 2003 by Sébastien Laoût <slaout@linux62.org>            *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License for more details.                          *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   along with this program; if not, write to the                         *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
+/**
+ * SPDX-FileCopyrightText: (C) 2003 by Sébastien Laoût <slaout@linux62.org>
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
 
 #include "aboutdata.h"
 #include <config.h>
@@ -38,8 +24,8 @@ AboutData::AboutData()
                      "https://basket.kde.org/")
 {
     //Pass basket.kde.org to constructor to be used as D-Bus domain name, but set the displayed address below
-    setHomepage("https://launchpad.net/basket");
-    setBugAddress("https://bugs.launchpad.net/basket");
+    setHomepage("https://cgit.kde.org/basket.git/");
+    setBugAddress("https://bugs.kde.org/buglist.cgi?component=general&list_id=1738678&product=basket&resolution=---");
 
     addAuthor(i18n("OmegaPhil"),
               i18n("Paste as plaintext option"),
