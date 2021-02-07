c981d4769730f80c11d79ade582db08a56ceefcf
Sebastian Engel <dev@sebastianengel.eu>
2021-02-07 23:42:14 +0100

    Welcome Baskets script: add xgettext to Message.sh

--- Messages.sh
+++ Messages.sh
@@ -2,3 +2,6 @@
 $EXTRACTRC `find . -name '*.ui' -o -name '*.rc' -o -name '*.ui'` >> rc.cpp
 $XGETTEXT `find . -name '*.cpp' -o -name '*.h'` -o $podir/basket.pot
 rm -f rc.cpp
+
+## Welcome baskets
+$XGETTEXT ./welcome/createWelcomeBaskets.py -o $podir/basketWelcome.pot
