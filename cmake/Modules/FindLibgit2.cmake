c8eedb5a8a3c53a9d386d897c80017aa78446297
Dominik Kummer <devel@arkades.org>
2024-11-05 14:04:17 +0100

    add libgit2 verison checking

--- cmake/Modules/FindLibgit2.cmake
+++ cmake/Modules/FindLibgit2.cmake
@@ -10,7 +10,7 @@
 # use pkg-config to get the directories and then use these values
 # in the FIND_PATH() and FIND_LIBRARY() calls
 FIND_PACKAGE(PkgConfig)
-PKG_CHECK_MODULES(PC_LIBGIT2 REQUIRED libgit2)
+PKG_SEARCH_MODULE(PC_LIBGIT2 libgit2)
 
 SET(LIBGIT2_DEFINITIONS ${PC_LIBGIT2_CFLAGS_OTHER})
 
@@ -26,8 +26,25 @@ FIND_LIBRARY(LIBGIT2_LIBRARIES NAMES git2
    ${PC_LIBGIT2_LIBRARY_DIRS}
 )
 
-
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(Libgit2 DEFAULT_MSG LIBGIT2_LIBRARIES LIBGIT2_INCLUDE_DIR)
 
 MARK_AS_ADVANCED(LIBGIT2_INCLUDE_DIR LIBGIT2_LIBRARIES)
+
+IF(Libgit2_FIND_VERSION)
+    # Retrieve the actual version
+    EXECUTE_PROCESS(
+        COMMAND pkg-config --modversion libgit2
+        OUTPUT_VARIABLE LIBGIT2_VERSION
+        OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+
+    # Check versions
+    IF(NOT LIBGIT2_VERSION VERSION_GREATER_EQUAL ${Libgit2_FIND_VERSION})
+      if ( NOT Libgit2_FIND_QUIETLY )
+        MESSAGE(WARNING "libgit2 version ${Libgit2_FIND_VERSION} or newer required, found ${LIBGIT2_VERSION}")
+      endif()
+      SET(LIBGIT2_FOUND FALSE)
+    ENDIF()
+ENDIF()
+
