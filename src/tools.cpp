KDE/basket

commit 2144aa18e51a973a24e46182edbca700b1c3f9e2

Remove some warnings

src/tools.cpp
@@ -810,6 +810,7 @@ QString Tools::makeStandardCaption(const QString &userCaption)
 QByteArray Tools::systemCodeset()
 {
     QByteArray codeset;
+#if 0 // TODO verify
 #if _LANGINFO_H
     // Qt since 4.2 always returns 'System' as codecForLocale and libraries like for example
     // KEncodingFileDialog expects real encoding name. So on systems that have langinfo.h use
@@ -822,6 +823,7 @@ QByteArray Tools::systemCodeset()
         // means ascii, "C"; QTextCodec doesn't know, so avoid warning
         codeset = "ISO-8859-1";
     }
+#endif
 #endif
     return codeset;
 }

src/notefactory.cpp
@@ -432,16 +432,16 @@ Note *NoteFactory::dropNote(const QMimeData *source, BasketScene *parent, bool f
     if (source->hasFormat(QStringLiteral("text/x-moz-url"))) { // FOR MOZILLA
         // Get the array and create a QChar array of 1/2 of the size
         QByteArray mozilla = source->data(QStringLiteral("text/x-moz-url"));
-        QVector<QChar> chars(mozilla.count() / 2);
+        QVector<QChar> chars(mozilla.size() / 2);
         // A small debug work to know the value of each bytes
         if (Global::debugWindow)
-            for (int i = 0; i < mozilla.count(); i++)
+            for (int i = 0; i < mozilla.size(); i++)
                 *Global::debugWindow << QStringLiteral("'") + QLatin1Char(mozilla[i]) + QStringLiteral("' ") + QString::number(int(mozilla[i]));
         // text/x-moz-url give the URL followed by the link title and separated by OxOA (10 decimal: new line?)
         uint size = 0;
         QChar *name = nullptr;
         // For each little endian mozilla chars, copy it to the array of QChars
-        for (int i = 0; i < mozilla.count(); i += 2) {
+        for (int i = 0; i < mozilla.size(); i += 2) {
             chars[i / 2] = QChar(mozilla[i], mozilla[i + 1]);
             if (mozilla.at(i) == 0x0A) {
                 size = i / 2;
@@ -516,7 +516,7 @@ Note *NoteFactory::createNoteUnknown(const QMimeData *source, BasketScene *paren
     // Echo the length (in bytes) and then the data, and then same for next MIME type:
     for (int i = 0; i < formats.size(); ++i) {
         QByteArray data = source->data(formats[i]);
-        stream << (quint32)data.count();
+        stream << (quint32)data.size();
         stream.writeRawData(data.data(), data.count());
     }
     file.close();

src/noteselection.cpp
@@ -46,9 +46,6 @@ NoteSelection *NoteSelection::nextStacked()
 
 NoteSelection *NoteSelection::firstStacked()
 {
-    if (!this)
-        return nullptr;
-
     if (note && note->content())
         return this;
     else
@@ -57,7 +54,7 @@ NoteSelection *NoteSelection::firstStacked()
 
 void NoteSelection::append(NoteSelection *node)
 {
-    if (!this || !node)
+    if (!node)
         return;
 
     if (firstChild) {
@@ -76,9 +73,6 @@ void NoteSelection::append(NoteSelection *node)
 
 int NoteSelection::count()
 {
-    if (!this)
-        return 0;
-
     int count = 0;
 
     for (NoteSelection *node = this; node; node = node->next)

commit 4515c97f6f74b702d45500dbe543edda8af2d5db

Drop unused/broken Tools::systemCodeset()
The only usage of it was recently dropped; it was silently broken by
commit 2144aa18e51a973a24e46182edbca700b1c3f9e2.

src/tools.cpp
@@ -37,8 +37,6 @@
 #include "htmlexporter.h"
 #include "linklabel.h"
 
-#include <langinfo.h>
-
 QVector<QTime> StopWatch::starts;
 QVector<double> StopWatch::totals;
 QVector<uint> StopWatch::counts;
@@ -805,25 +803,4 @@ QString Tools::makeStandardCaption(const QString &userCaption)
     }
 
     return caption;
-}
-
-QByteArray Tools::systemCodeset()
-{
-    QByteArray codeset;
-#if 0 // TODO verify
-#if _LANGINFO_H
-    // Qt since 4.2 always returns 'System' as codecForLocale and libraries like for example
-    // KEncodingFileDialog expects real encoding name. So on systems that have langinfo.h use
-    // nl_langinfo instead, just like Qt compiled without iconv does. Windows already has its own
-    // workaround
-
-    codeset = nl_langinfo(CODESET);
-
-    if ((codeset == "ANSI_X3.4-1968") || (codeset == "US-ASCII")) {
-        // means ascii, "C"; QTextCodec doesn't know, so avoid warning
-        codeset = "ISO-8859-1";
-    }
-#endif
-#endif
-    return codeset;
 }

src/tools.h
@@ -124,9 +124,6 @@ BASKET_EXPORT qint64 computeSizeRecursively(const QString &path);
  */
 BASKET_EXPORT bool isAFileCut(const QMimeData *source);
 
-/// Implementation of system encoding detection from KDE 4
-BASKET_EXPORT QByteArray systemCodeset();
-
 // Debug
 BASKET_EXPORT void printChildren(QObject *parent);
 }